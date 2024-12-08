c3b6f6c6200959e9a1757629e39640c1f2ff0d41
Christophe335 <christophemouillet335@hotmail.com>
2024-12-08 10:29:31 +0100

    mise en place derniers fichiers à ce stade

---
+++ script.js
@@ -0,0 +1,7 @@
+function openModal(type, id) {
+    alert("Ouvrir la modal pour " + type + " de l'article " + id);
+}
+
+function ajouterAuPanier(id) {
+    alert("Article " + id + " ajouté au panier.");
+}
