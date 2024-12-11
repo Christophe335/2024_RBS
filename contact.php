529b2bb46d5a0569c7a1eb4fb1bdd6baeb28fa7f
Christophe335 <christophemouillet335@hotmail.com>
2024-12-11 22:23:45 +0100

    création de la page contact mais vide, seulement le header et le footer

---
+++ contact.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>header</title>
+    <link rel="stylesheet" href="../style.css">
+</head>
+<body>
+<?php include 'db_connect.php'; ?>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<header>
+<?php include 'header.php' ?>
+
+ <div class="espace"></div>
+<?php include 'footer.php' ?>
+</body>
+</html>
